120883c6a340fbfe8bf7b0c873efbc8a0aa0cf6e
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2021-11-29 14:07:31 -0500

    Update script

--- scripts/read_infoclimat_data.py
+++ scripts/read_infoclimat_data.py
@@ -7,14 +7,19 @@ Created on Fri Oct 22 15:33:13 2021
 import netCDF4
 import numpy as np
 
-filepath = 'D:/Data/InfoClimatNew/NetCDF/PREC_1984.nc'
+filepath = 'D:/Data/GrilleInfoClimat2021/PREC_1985.nc'
 netcdf_dset = netCDF4.Dataset(filepath, 'r+')
 
 print(netcdf_dset)
+
+x = np.array(netcdf_dset['x'])
+y = np.array(netcdf_dset['y'])
 
 latitude = np.array(netcdf_dset['lat'])
 longitude = np.array(netcdf_dset['lon'])
 time = np.array(netcdf_dset['time'])
 prec = np.array(netcdf_dset['PREC'])
+
+netcdf_dset.title
 
 netcdf_dset.close()
